d011f48479912efbfdeef38250807c6a5a7ac181
PlayMaks <maks1997clan@gmail.com>
2023-10-19 23:17:57 +0300

    Init generation DB Data

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -8,7 +8,9 @@ import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -23,71 +25,69 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner init(UserRepository userRepository,
-                           InvitationRepository invitationRepository,
-                           KeywordRepository keywordRepository) {
-        return args -> {
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup(UserRepository userRepository,
+                                        InvitationRepository invitationRepository,
+                                        KeywordRepository keywordRepository) {
+        System.out.println("hello world, I have just started up");
 
-            // Ініціалізуємо генератор випадкових даних
-            Faker faker = new Faker();
+        // Ініціалізуємо генератор випадкових даних
+        Faker faker = new Faker();
 
-            // Створюємо користувачів
-            List<User> users = new ArrayList<>();
-            for (int i = 0; i < 10; i++) {
-                User user = new User();
-                user.setUsername(faker.name().username());
-                user.setPassword(faker.internet().password());
+        // Створюємо користувачів
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            User user = new User();
+            user.setUsername(faker.name().username());
+            user.setPassword(faker.internet().password());
 
-                OpenInfo openInfo = new OpenInfo();
-                openInfo.setFirstName(faker.name().firstName());
-                openInfo.setLastName(faker.name().lastName());
-                openInfo.setLocation(faker.address().city());
-                openInfo.setDescription(faker.lorem().sentence());
-                user.setOpenInfo(openInfo);
+            OpenInfo openInfo = new OpenInfo();
+            openInfo.setFirstName(faker.name().firstName());
+            openInfo.setLastName(faker.name().lastName());
+            openInfo.setLocation(faker.address().city());
+            openInfo.setDescription(faker.lorem().sentence());
+            user.setOpenInfo(openInfo);
 
-                SecretInfo secretInfo = new SecretInfo();
-                secretInfo.setEmail(faker.internet().emailAddress());
-                secretInfo.setPhone(faker.phoneNumber().phoneNumber());
-                user.setSecretInfo(secretInfo);
+            SecretInfo secretInfo = new SecretInfo();
+            secretInfo.setEmail(faker.internet().emailAddress());
+            secretInfo.setPhone(faker.phoneNumber().phoneNumber());
+            user.setSecretInfo(secretInfo);
 
-                users.add(user);
+            users.add(user);
+        }
+
+        // Створюємо ключові слова
+        List<Keyword> keywords = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            Keyword keyword = new Keyword();
+            keyword.setValue(faker.lorem().word());
+            keywords.add(keyword);
+        }
+
+        // Додаємо ключові слова користувачам
+        for (User user : users) {
+            Set<Keyword> userKeywords = new HashSet<>();
+            for (int i = 0; i < 5; i++) {
+                userKeywords.add(keywords.get(faker.random().nextInt(keywords.size())));
             }
+            user.setKeywords(userKeywords);
+        }
 
-            // Створюємо ключові слова
-            List<Keyword> keywords = new ArrayList<>();
-            for (int i = 0; i < 20; i++) {
-                Keyword keyword = new Keyword();
-                keyword.setValue(faker.lorem().word());
-                keywords.add(keyword);
-            }
+        // Створюємо запрошення
+        List<Invitation> invitations = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            Invitation invitation = new Invitation();
+            invitation.setSender(users.get(faker.random().nextInt(users.size())));
+            invitation.setReceiver(users.get(faker.random().nextInt(users.size())));
+            invitation.setMessage(faker.lorem().sentence());
+            invitation.setSentDateTime(LocalDateTime.now().minusDays(faker.number().numberBetween(1, 30)));
+            invitations.add(invitation);
+        }
 
-            // Додаємо ключові слова користувачам
-            for (User user : users) {
-                Set<Keyword> userKeywords = new HashSet<>();
-                for (int i = 0; i < 5; i++) {
-                    userKeywords.add(keywords.get(faker.random().nextInt(keywords.size())));
-                }
-                user.setKeywords(userKeywords);
-            }
-
-            // Створюємо запрошення
-            List<Invitation> invitations = new ArrayList<>();
-            for (int i = 0; i < 30; i++) {
-                Invitation invitation = new Invitation();
-                invitation.setSender(users.get(faker.random().nextInt(users.size())));
-                invitation.setReceiver(users.get(faker.random().nextInt(users.size())));
-                invitation.setMessage(faker.lorem().sentence());
-                invitation.setSentDateTime(LocalDateTime.now().minusDays(faker.number().numberBetween(1, 30)));
-                invitations.add(invitation);
-            }
-
-            // Зберігаємо користувачів, ключові слова та запрошення
-            userRepository.saveAll(users);
-            keywordRepository.saveAll(keywords);
-            invitationRepository.saveAll(invitations);
-
-        };
+        // Зберігаємо користувачів, ключові слова та запрошення
+        userRepository.saveAll(users);
+        keywordRepository.saveAll(keywords);
+        invitationRepository.saveAll(invitations);
     }
-
 }
+
