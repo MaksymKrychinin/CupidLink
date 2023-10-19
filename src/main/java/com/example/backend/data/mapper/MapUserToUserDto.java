dc29cd777bfc0c885e05d12eba2467235506db65
Zakhar Zakharchuk <Zakhar_Zakharchuk@epam.com>
2023-10-19 09:55:10 +0300

    update mappers

---
+++ src/main/java/com/example/backend/data/mapper/MapUserToUserDto.java
@@ -0,0 +1,8 @@
+package com.example.backend.data.mapper;
+
+import org.mapstruct.Qualifier;
+
+@Qualifier
+public @interface MapUserToUserDto {
+
+}
--- src/main/java/com/example/backend/data/mapper/UserMapper.java
+++ src/main/java/com/example/backend/data/mapper/UserMapper.java
@@ -2,10 +2,25 @@ package com.example.backend.data.mapper;
 
 import com.example.backend.data.dto.UserDto;
 import com.example.backend.data.entity.User;
+import java.util.Set;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
-@Mapper
+@Mapper(componentModel = "spring", uses = {OpenInfoMapper.class, SecretInfoMapper.class,
+      KeywordMapper.class})
 public interface UserMapper {
+
     User toUser(UserDto userDto);
+
+    @MapUserToUserDto
+    @Mappings({
+          @Mapping(source = "id", target = "id"),
+          @Mapping(source = "openInfo", target = "openInfo"),
+          @Mapping(source = "keywords", target = "keywords"),
+          @Mapping(target = "secretInfo", ignore = true)})
     UserDto toUserDto(User user);
+
+    UserDto toUserDtoWithSecretInfo(User user);
+    Set<UserDto> toUserDtoSet(Set<User> users);
 }
