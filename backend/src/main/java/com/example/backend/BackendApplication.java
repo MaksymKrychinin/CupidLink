3ff7fc5cc2e8549b704005ca78b1f521e533febd
PlayMaks <maks1997clan@gmail.com>
2023-10-20 12:11:52 +0300

    DataAutoGeneration

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -5,10 +5,12 @@ import com.example.backend.repository.InvitationRepository;
 import com.example.backend.repository.KeywordRepository;
 import com.example.backend.repository.UserRepository;
 import com.github.javafaker.Faker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 
@@ -20,16 +22,20 @@ import java.util.Set;
 
 @SpringBootApplication
 public class BackendApplication {
+    @Autowired
+    private ApplicationContext context;
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
 
-   /* @EventListener(ApplicationReadyEvent.class)
-    public void doSomethingAfterStartup(UserRepository userRepository,
-                                        InvitationRepository invitationRepository,
-                                        KeywordRepository keywordRepository) {
-        System.out.println("hello world, I have just started up");
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup() {
+        UserRepository userRepository = context.getBean(UserRepository.class);
+        InvitationRepository invitationRepository = context.getBean(InvitationRepository.class);
+        KeywordRepository keywordRepository = context.getBean(KeywordRepository.class);
+
+
 
         // Ініціалізуємо генератор випадкових даних
         Faker faker = new Faker();
@@ -88,6 +94,8 @@ public class BackendApplication {
         userRepository.saveAll(users);
         keywordRepository.saveAll(keywords);
         invitationRepository.saveAll(invitations);
-    }*/
+
+        System.out.println("SUCCSESS STARTUP APPLICATION READY EVENT LISTENER");
+    }
 }
 
