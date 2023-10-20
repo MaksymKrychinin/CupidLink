fe68aa6e4d51603694a2fb1ffae60a25923642cd
PlayMaks <maks1997clan@gmail.com>
2023-10-20 11:50:02 +0300

    Swagger added/No_Content controller response openApiAnnotated

--- backend/src/main/java/com/example/backend/controller/InvitationController.java
+++ backend/src/main/java/com/example/backend/controller/InvitationController.java
@@ -2,8 +2,12 @@ package com.example.backend.controller;
 
 import com.example.backend.data.dto.InvitationDto;
 import com.example.backend.service.InvitationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -26,11 +30,31 @@ public class InvitationController {
         return invitationService.getSentInvitationsByReceiverId(id, pageNum);
     }
 
+    @Operation(
+            summary = "Send invitation to",
+            description = "Send invitation to"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "400", description = "Invalid parameters or other bad request"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PostMapping("/")
     public void sendInvitation(@RequestBody InvitationDto invitationDto) {
         invitationService.sendInvitation(invitationDto);
     }
 
+    @Operation(
+            summary = "Delete Invitation by ID",
+            description = "Delete Invitation by ID"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "400", description = "Invalid parameters or other bad request"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void deleteInvitation(@PathVariable Long id, Long currentUserId) {
         invitationService.deleteInvitation(id, currentUserId);
--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -3,9 +3,12 @@ package com.example.backend.controller;
 import com.example.backend.data.dto.UserDto;
 import com.example.backend.data.entity.User;
 import com.example.backend.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,6 +17,16 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserService userService;
 
+    @Operation(
+            summary = "Create user",
+            description = "Create user with given userDto"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "400", description = "Invalid parameters or other bad request"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PostMapping("/")
     public void createUser(@RequestBody UserDto userDto) {
         userService.createUser(userDto);
