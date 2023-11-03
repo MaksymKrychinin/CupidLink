9a33ba2acec610bf39a281f40129de6b7c56224f
Zakhar Zakharchuk <Zakhar_Zakharchuk@epam.com>
2023-11-03 09:24:16 +0200

    Validated

--- backend/src/main/java/com/example/backend/controller/InvitationController.java
+++ backend/src/main/java/com/example/backend/controller/InvitationController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/invitation")
 @RequiredArgsConstructor
+@Validated
 public class InvitationController {
 
     private final InvitationService invitationService;
