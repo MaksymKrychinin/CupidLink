37af12b373493658a1f340c384d5584e4c689821
Zakhar Zakharchuk <Zakhar_Zakharchuk@epam.com>
2023-11-03 09:31:19 +0200

    Validation

--- backend/src/main/java/com/example/backend/data/dto/OpenInfoDto.java
+++ backend/src/main/java/com/example/backend/data/dto/OpenInfoDto.java
@@ -1,5 +1,7 @@
 package com.example.backend.data.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +13,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class OpenInfoDto {
     private Long id;
+    @NotNull(message = "First name cannot be null")
+    @NotEmpty(message = "First name cannot be empty")
     private String firstName;
+    @NotNull(message = "Last name cannot be null")
+    @NotEmpty(message = "Last name cannot be empty")
     private String lastName;
     private String location;
     private String description;
--- backend/src/main/java/com/example/backend/data/dto/RegisterRequest.java
+++ backend/src/main/java/com/example/backend/data/dto/RegisterRequest.java
@@ -3,6 +3,9 @@ package com.example.backend.data.dto;
 
 import com.example.backend.data.entity.OpenInfo;
 import com.example.backend.data.entity.SecretInfo;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,7 +17,12 @@ import lombok.NoArgsConstructor;
 @Builder
 public class RegisterRequest {
     private Long id;
+    @NotNull(message = "Username cannot be null")
+    @NotEmpty(message = "Username cannot be empty")
     private String username;
+    @NotNull(message = "Password cannot be null")
+    @NotEmpty(message = "Password cannot be empty")
+    @Min(value = 8, message = "Password must be at least 8 characters")
     private String password;
     private OpenInfo openInfo;
     private SecretInfo secretInfo;
